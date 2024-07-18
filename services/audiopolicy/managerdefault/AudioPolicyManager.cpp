0881647a7bd066bb2c439a13d9e75a9e43ac7139
Mikhail Naganov <mnaganov@google.com>
2024-07-18 16:01:54 +0000

    Revert^2 "audiopolicy: use profile flags during opening an input"
    This reverts commit a31591a22191df722f565ea6fa197ad7c15e5e16
    (aosp/3168898).
    
    Reason for revert: The underlying b/352789444 that was causing
    device-boot-health-check-extra to fail was fixed.
    
    Flag: EXEMPT bugfix
    Bug: 345692843
    Bug: 352575543
    Bug: 352789444
    Test: connect an external device, check opened inputs, verify logs.
    Change-Id: I9d3ba12ae23bc94a80282886fdbb3d6c3d35a702

--- services/audiopolicy/managerdefault/AudioPolicyManager.cpp
+++ services/audiopolicy/managerdefault/AudioPolicyManager.cpp
@@ -6480,7 +6480,7 @@ void AudioPolicyManager::onNewAudioModulesAvailableInt(DeviceVector *newDevices)
             status_t status = inputDesc->open(nullptr,
                                               availProfileDevices.itemAt(0),
                                               AUDIO_SOURCE_MIC,
-                                              AUDIO_INPUT_FLAG_NONE,
+                                              (audio_input_flags_t) inProfile->getFlags(),
                                               &input);
             if (status != NO_ERROR) {
                 ALOGW("%s: Cannot open input stream for device %s for profile %s on hw module %s",
@@ -6795,7 +6795,8 @@ status_t AudioPolicyManager::checkInputsForDevice(const sp<DeviceDescriptor>& de
             desc = new AudioInputDescriptor(profile, mpClientInterface);
             audio_io_handle_t input = AUDIO_IO_HANDLE_NONE;
             ALOGV("%s opening input for profile %s", __func__, profile->getTagName().c_str());
-            status = desc->open(nullptr, device, AUDIO_SOURCE_MIC, AUDIO_INPUT_FLAG_NONE, &input);
+            status = desc->open(nullptr, device, AUDIO_SOURCE_MIC,
+                                (audio_input_flags_t) profile->getFlags(), &input);
 
             if (status == NO_ERROR) {
                 const String8& address = String8(device->address().c_str());
