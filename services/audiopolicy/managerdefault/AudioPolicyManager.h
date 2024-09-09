8558a3374477a63c846a9979c4b357ad9035a462
wenyu zhang <zhangwenyu@google.com>
2024-09-09 15:12:48 +0000

    Reuse getDevicesForAttributes for get input devices
    Change-Id: Ib467fcdf3aa1ae5cb71375c773bfda09c89e669c
    Bug: b/364923030, b/357122624
    Flag: com.android.media.audioserver.enable_audio_input_device_routing

--- services/audiopolicy/managerdefault/AudioPolicyManager.h
+++ services/audiopolicy/managerdefault/AudioPolicyManager.h
@@ -1365,6 +1365,11 @@ private:
                                          DeviceVector &devices,
                                          bool forVolume);
 
+        // A helper method used by getDevicesForAttributes to retrieve input devices when
+        // capture preset is available in the given audio attributes parameter.
+        status_t getInputDevicesForAttributes(const audio_attributes_t &attr,
+                                              DeviceVector &devices);
+
         status_t getProfilesForDevices(const DeviceVector& devices,
                                        AudioProfileVector& audioProfiles,
                                        uint32_t flags,
