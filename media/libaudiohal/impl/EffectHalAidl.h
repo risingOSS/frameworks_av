688613e28573cac3c3f11b183d9579933dedf3d8
Shunkai Yao <yaoshunkai@google.com>
2024-09-12 23:57:55 +0000

    EffectHalAidl: fix the input/output buffer reading size
    the actual framecount in EffectBufferHalInterface could be smaller than
    allocated buffer size
    cleanup and split the process code
    
    Flag: EXEMPT bugfix
    Bug: 363428226
    Test: atest AudioEffectTest CtsMediaAudioTestCases audioeffect_analysis
    Test: HapticGenerator testing APP
    
    Change-Id: I1f2808b20fe084935e0b77a38b8b0d4ed9b52f6b
    Merged-In: I1f2808b20fe084935e0b77a38b8b0d4ed9b52f6b

--- media/libaudiohal/impl/EffectHalAidl.h
+++ media/libaudiohal/impl/EffectHalAidl.h
@@ -73,7 +73,9 @@ class EffectHalAidl : public EffectHalInterface {
     const int32_t mSessionId;
     const int32_t mIoId;
     const bool mIsProxyEffect;
+    const int mHalVersion;
     bool mIsHapticGenerator = false;
+    std::string mEffectName;
 
     std::unique_ptr<EffectConversionHelperAidl> mConversion;
 
@@ -92,6 +94,14 @@ class EffectHalAidl : public EffectHalInterface {
             bool isProxyEffect);
     bool setEffectReverse(bool reverse);
     bool needUpdateReturnParam(uint32_t cmdCode);
+
+    status_t maybeReopen(const std::shared_ptr<android::hardware::EventFlag>& efGroup) const;
+    void writeHapticGeneratorData(size_t totalSamples, float* const outputRawBuffer,
+                                  float* const fmqOutputBuffer) const;
+    size_t writeToHalInputFmqAndSignal(
+            const std::shared_ptr<android::hardware::EventFlag>& efGroup) const;
+    status_t waitHalStatusFmq(size_t samplesWritten) const;
+    status_t readFromHalOutputFmq(size_t samplesWritten) const;
 
     // The destructor automatically releases the effect.
     virtual ~EffectHalAidl();
